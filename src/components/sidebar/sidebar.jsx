0cdaffa607fa6ceb91f4d17c3ed2eae8af60de63
Puka-jpg <pukar369@gmail.com>
2025-01-10 22:39:42 +0545

    Added search functionality

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,9 +1,18 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 
 import "./sidebar.css";
 import { assets } from "../../assets/assets";
+import { Context } from "../../context/context";
 const Sidebar = () => {
   const [extended, setExtended] = useState(false);
+  const { onSent, previousPrompts, setRecentPrompt, newChat } =
+    useContext(Context); // corrected destructuring
+
+  const loadPrompt = async (prompt) => {
+    setRecentPrompt(prompt);
+
+    await onSent(prompt);
+  };
 
   return (
     <div className="sidebar">
@@ -14,7 +23,7 @@ const Sidebar = () => {
           src={assets.menu_icon}
         />
 
-        <div className="new-chat">
+        <div onClick={newChat} className="new-chat">
           <img src={assets.plus_icon} alt="" />
           {extended ? <p>New Chat</p> : null}
         </div>
@@ -22,10 +31,14 @@ const Sidebar = () => {
         {extended ? (
           <div className="recent">
             <p className="recent-title">Recent</p>
-            <div className="recent-entry">
-              <img src={assets.message_icon} />
-              <p>What is react...</p>
-            </div>
+            {previousPrompts.map((item, index) => {
+              return (
+                <div onClick={() => loadPrompt(item)} className="recent-entry">
+                  <img src={assets.message_icon} />
+                  <p>{item.slice(0, 18)} ...</p>
+                </div>
+              );
+            })}
           </div>
         ) : null}
       </div>
